dca1167279add76689357ed23335d2d6535fce86
vaibhav <vaibhavwadhwa84@gmail.com>
2023-11-21 00:47:04 +0530

    Committed all the files

---
+++ index.js
@@ -0,0 +1,8 @@
+// Get the image card element
+const imageCard = document.getElementById("image-card");
+
+// Add a click event listener to the image card
+imageCard.addEventListener("click", function() {
+  // Redirect to a new HTML page when the image card is clicked
+  window.location.href = "path-to-your-new-html-page.html";
+});
